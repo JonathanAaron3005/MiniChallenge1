d8435179528f5ac09fdf53ad91c1c9734a84ba1f
Jonathan Aaron Wibawa <aaron.wibawa@gmail.com>
2024-03-27 13:13:38 +0700

    Initial Commit

---
+++ Mini1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mini1
+//
+//  Created by Jonathan Aaron Wibawa on 27/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mini1/Mini1App.swift
@@ -0,0 +1,17 @@
+//
+//  Mini1App.swift
+//  Mini1
+//
+//  Created by Jonathan Aaron Wibawa on 27/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Mini1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
