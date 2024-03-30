7c01d7675018c53aae729fe869770b2ec72480a4
Jonathan Aaron Wibawa <aaron.wibawa@gmail.com>
2024-03-30 14:43:16 +0700

    added TabView

---
+++ Mini1/PlanView.swift
@@ -0,0 +1,18 @@
+//
+//  SomeOtherView.swift
+//  Mini1
+//
+//  Created by Jonathan Aaron Wibawa on 30/03/24.
+//
+
+import SwiftUI
+
+struct PlanView: View {
+    var body: some View {
+        Text("Plan View")
+    }
+}
+
+#Preview {
+    PlanView()
+}
